57323ac8db36cc5c41e3b885078b350885ce95d6
junwoo <mkshie@naver.com>
2025-12-24 21:06:26 +0900

    feat : StationRepository 에 Map 추가하여 이름과 Station 매핑 추가

--- src/main/java/subway/domain/StationRepository.java
+++ src/main/java/subway/domain/StationRepository.java
@@ -2,11 +2,15 @@ package subway.domain;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class StationRepository {
     private static final List<Station> stations = new ArrayList<>();
+    private static Map<String , Station> stationMap = new HashMap<>();
 
     public static List<Station> stations() {
         return Collections.unmodifiableList(stations);
@@ -14,6 +18,17 @@ public class StationRepository {
 
     public static void addStation(Station station) {
         stations.add(station);
+    }
+
+    public static void SetUpStationsMap(){
+        stationMap = stations.stream().collect(Collectors.toMap(Station::getName , stations -> stations));
+    }
+
+    public static Station findStationByName(String name) {
+        if(name == null || !stationMap.containsKey(name)) {
+            throw new IllegalArgumentException("존재하지 않는 역 이름입니다.");
+        }
+        return stationMap.get(name);
     }
 
     public static boolean deleteStation(String name) {
