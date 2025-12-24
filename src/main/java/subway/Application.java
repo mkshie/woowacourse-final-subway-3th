4eb0625ffecfd0ee5fa6c393a28ae4dd6114dbb2
junwoo <mkshie@naver.com>
2025-12-24 21:07:26 +0900

    feat : 기본 함수 호출

--- src/main/java/subway/Application.java
+++ src/main/java/subway/Application.java
@@ -1,10 +1,23 @@
 package subway;
 
 import java.util.Scanner;
+import subway.controller.SubwayController;
+import subway.init.SubwayInitializer;
+import subway.service.SubwayService;
+import subway.view.InputView;
+import subway.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
-        // TODO: 프로그램 구현
+        InputView inputView = new InputView(scanner);
+        OutputView outputView = new OutputView();
+        SubwayInitializer subwayInitializer = new SubwayInitializer();
+        subwayInitializer.init();
+
+        SubwayService subwayService = new SubwayService();
+
+        SubwayController subwayController = new SubwayController(inputView,outputView , subwayService);
+        subwayController.run();
     }
 }
